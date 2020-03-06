e796a032cf9161e03f9fceb22f47178a1e45551f
rootFaheem <faheemtech1@gmail.com>
2020-03-06 19:03:02 +0530

    port mapping is working

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express();
 
 app.get("/", (req, res, next) => res.send("Hi There"));
 
-app.listen(8000, () => console.log("🚀 server running at 8000"));
+app.listen(8080, () => console.log("🚀 server running at 8080"));
