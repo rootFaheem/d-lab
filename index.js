a30b5a49846335dfef78fe45a144e2c200604576
rootFaheem <faheemtech1@gmail.com>
2020-03-07 20:56:21 +0530

    docker is working fine

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res, next) => res.send("How are you?"));
+app.get("/", (req, res, next) =>
+  res.send("How are you? Is this really working")
+);
 
 app.listen(8080, () => console.log("🚀 server running at 8080"));
