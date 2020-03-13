804a40c7e8c28322df01d3323abc0c71cf8d9c38
rootFaheem <faheemtech1@gmail.com>
2020-03-13 19:58:52 +0530

    express-redis app ready

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const redis = require("redis");
 
 const app = express();
+const client = redis.createClient();
+
+client.set("visits", 0);
 
 app.get("/", (req, res, next) =>
-  res.send("How are you? Is this really working")
+  res.status(200).send("How are you? Is this really working")
 );
+
+app.get("/visits", (req, res, next) => {
+  client.get("visits", (err, visits) => {
+    res.status(200).send(`Total Visits: ${visits}`);
+    client.set("visits", parseInt(visits) + 1);
+  });
+});
 
 app.listen(8080, () => console.log("🚀 server running at 8080"));
