366b8f7b11026baba8c8b061041328f6b7a63c13
rootFaheem <faheemtech1@gmail.com>
2020-03-16 16:39:59 +0530

    test is not live updating

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from my page
+          I was changed again
         </a>
       </header>
     </div>
