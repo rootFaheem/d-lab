ceaac2c60feb47e7cac8dd7c91b7bb8ff584f687
rootFaheem <faheemtech1@gmail.com>
2020-03-18 00:11:22 +0530

    text cahnges

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Macfa App</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed again
+          Click to Learn More
         </a>
       </header>
     </div>
