a3dca5ff3322e2b8f6a5cd6cbc3ad11b246c88b2
rootFaheem <faheemtech1@gmail.com>
2020-03-18 00:41:03 +0530

    latest packages deployed

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import React from "react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/I was/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test("renders learn react link", () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/I was/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
